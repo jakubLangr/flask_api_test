5a581d179215b2bd9b1b724b041259cb1348d2fc
Jakub Langr <james.langr@gmail.com>
2015-12-11 15:10:08 +0000

    Working filterReplies POST and GET

--- server.py
+++ server.py
@@ -14,18 +14,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 
 db = SQLAlchemy(app)
 
-def check_presence(data_attr, item):
-    if data_attr.UserId==item:
-        return item
-    else:
-        pass
-
-
 class ValidationError(ValueError):
     pass
 
 class Module(db.Model):
     __tablename__ = 'modules'
+    __table_args__ = { 'sqlite_autoincrement': True }
     id = db.Column( db.Integer, primary_key=True )
     UserId = db.Column( db.String(255) ) 
     CourseSoftwareId = db.Column( db.String(255) )
@@ -60,7 +54,7 @@ class Module(db.Model):
             CREATE TABLE modules (
                  id integer PRIMARY KEY NOT NULL,
                  UserId integer NOT NULL,
-                 CourseSoftwareId NOT NULL,
+                 CourseSoftwareId NOT NULL, 
                  CourseMaterialId NOT NULL,
                  N2K float,
                  DAK float,
@@ -70,7 +64,7 @@ class Module(db.Model):
                  ModifiedDate datetime
                  ); '''
         try:
-            self.id = data['id']
+            # self.id = data['id']
             self.UserId = data['UserId']
             self.CourseSoftwareId = data['CourseSoftwareId']
             self.CourseMaterialId = data['CourseMaterialId']
@@ -86,30 +80,87 @@ class Module(db.Model):
             raise ValidationError('Invalid row (module), missing ' + e.args[0] )
         return self
 
+
+class FilterReply(db.Model):
+    # waypoints for standalone filter and rough timelines 
+    # draw out conclusions & snapshots 
+    # live demo of Flask API?
+    __tablename__ = 'filterReplies'
+    id = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.String(64), index=True)
+    CourseSoftwareId = db.Column(db.String(64))
+    Type = db.Column(db.String(64))
+    Answer = db.Column(db.String(64))
+    MaxAnswer = db.Column(db.String(64))
+    CreatedDate = db.Column(db.DateTime, default=datetime.now)
+    ModifiedDate = db.Column(db.DateTime, default=datetime.now)
+
+    def get_url(self):
+        return url_for('get_filter_reply', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url' : self.get_url(),
+            'id' : self.id,
+            'UserId': self.UserId,
+            'CourseSoftwareId' : self.CourseSoftwareId,
+            'Type' : self.Type,
+            'Answer' : self.Answer,
+            'MaxAnswer' : self.MaxAnswer,
+            'CreatedDate'  : self.CreatedDate.isoformat() + 'Z',
+            'ModifiedDate' : self.ModifiedDate.isoformat() + 'Z'
+        }
+
+    def import_data(self, data):
+        try:
+            ''' Schema
+            CREATE TABLE filterReplies ( 
+                id Integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                UserId String NOT NULL,
+                CourseSoftwareId string NOT NULL,
+                Type String,
+                Answer Float,
+                MaxAnswer Integer,
+                CreatedDate datetime,
+                ModifiedDate datetime);'''
+
+            # self.id = data['id']
+            self.UserId = data['UserId']
+            self.CourseSoftwareId = data['CourseSoftwareId']
+            self.Type = data['Type']
+            self.Answer = data['Answer']
+            self.MaxAnswer = data['MaxAnswer']
+            self.CreatedDate = datetime_parser.parse(
+                data['CreatedDate'])
+            self.ModifiedDate = datetime_parser.parse(
+                data['ModifiedDate'])
+        except KeyError as e:
+            raise ValidationError('Invalid row (module), missing ' + e.args[0] )
+        return self
+
+
+@app.route('/filterReplies/', methods=['POST'])
+def new_filter_reply():
+    '''
+http POST http://localhost:5000/filterReplies/ id=1 UserId=aaaa CourseSoftwareId=aaaaa Type=AOI Answer=5 MaxAnswer=12 CreatedDate='22 Jan 2013' ModifiedDate='23 Jun 2013'
+    '''
+    filter_reply = FilterReply()
+    filter_reply.import_data(request.json)
+    db.session.add(filter_reply)
+    db.session.commit()
+    return jsonify({}), 201, {'Location' : filter_reply.get_url() }
+
+@app.route('/filterReplies/<int:id>', methods=['GET'])
+def get_filter_reply(id):
+    return jsonify(FilterReply.query.get_or_404(id).export_data())
+
 @app.route('/modules/', methods=['GET'])
 def get_modules():
     return jsonify({'modules' : [ module.get_url() for module in Module.query.all() ] })
 
 @app.route('/modules/<string:UserId>', methods=['GET'])
 def get_module(UserId):
-    # the call here passes UserId correctly, investigate get_or_404 method 
-    # [SQL: u'SELECT dummy.id AS dummy_id \nFROM dummy \nWHERE dummy.id = ?'] [parameters: ('hi',)]
-    # SELECT modules.UserId AS module_UserId FROM modules WHERE modules.UserId = 'aaac';
-    # Turns out get_or_404 uses the primary key only, so I cannot query on UserId
-    # response =  Module.query.all()
-    # response = map(check_presence(response, UserId), response)
-    # response = {'modules' : [ module for module in Module.query.all() ] }
-    # Module.query.filter(Module.UserId==UserId)
-    ''' Module.UserId == UserId
-    all_modules = Module.query.all()
-    response = []
-    for module in all_modules:
-        if module.UserId == UserId:
-            response.append(module)
-    return response
-    '''
     results = [ (module.id,module.export_data()) for module in Module.query.all() if module.UserId==UserId ]  
-    print results
     return jsonify( results )
 
 @app.route('/modules/<int:id>', methods=['PUT'])
